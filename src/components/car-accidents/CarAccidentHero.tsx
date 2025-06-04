cff8391bcd006c932a49287bb94d184eabdc0cc6
Chandan Gupta <chandangupta@Chandans-MacBook-Air.local>
2025-06-05 04:22:13 +0530

    Update image sources to use webp format and clean up comments in car accident components

--- src/components/car-accidents/CarAccidentHero.tsx
+++ src/components/car-accidents/CarAccidentHero.tsx
@@ -1,4 +1,3 @@
-// src/components/car-accidents/CarAccidentHero.tsx
 "use client";
 import styled from "styled-components";
 
@@ -6,7 +5,7 @@ const HeroContainer = styled.section`
   position: relative;
   width: 100%;
 
-  background-image: url("/images/car-accident-banner.jpg");
+  background-image: url("/images/car-accident-banner.webp");
   background-size: cover;
   background-position: center;
   background-color: rgba(27, 38, 50, 0.7);
@@ -91,7 +90,6 @@ const HeroTitle = styled.h1`
   }
 `;
 
-// New styled components for the title parts
 const TitleFirstPart = styled.span`
   @media (max-width: 768px) {
     display: block;
